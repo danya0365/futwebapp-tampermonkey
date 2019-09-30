d7b18e0d3918da1d88240213f1874801edf86d92
Marosdee <marosdee@isidsea.com>
2019-09-30 15:49:02 +0700

    Implement Show Google Link.

--- app/enhance-overral-ui/enhance-overral-ui.js
+++ app/enhance-overral-ui/enhance-overral-ui.js
@@ -25,16 +25,13 @@ export class EnhanceOverralUi extends BaseScript {
     super.onScreenRequest(screenId);
     const settings = this.getSettings();
 
-    if (settings.isActive) {
-
-    }
   }
 
   _mutationHandler(mutationRecords) {
     mutationRecords.forEach(function (mutation) {
       if ($(mutation.target).hasClass('DetailView') && $(mutation.target)
         .find('.DetailPanel') && mutation.addedNodes.length > 0) {
-        if (this.getSettings()['show-link-to-player'].toString() !== 'true') {
+        if (this.getSettings()['show-google-link'].toString() !== 'true') {
           return;
         }
 
@@ -43,9 +40,47 @@ export class EnhanceOverralUi extends BaseScript {
           return;
         }
 
-        const futbinPlayerLink = $(mutation.target).find('#futbinPlayerLink');
+        const googleLink = $(mutation.target).find('#googleLink');
+        googleLink.remove();
+
+        $(mutation.target).find('.DetailPanel > .ut-button-group').prepend(`<button id="googleLink" data-resource-id="${selectedItem.resourceId}" class="list"><span class="btn-text">Google Link</span><span class="btn-subtext"></span></button>`);
+
+        $('#googleLink').bind('click', async () => {
+          let btn = $('#googleLink');
+          btn.find('.btn-text').html('Searching on Google ...');
+
+          selectedItem = this._getSelectedItem();
+          btn = $('#googleLink');
+          if (btn.data('resource-id') === selectedItem.resourceId) {
+              btn.find('.btn-text').html('Google Link');
+              analytics.trackEvent('Test', 'Google Link', btn.data('resource-id'));
+              window.open("https://www.google.com/?resource-id=" + btn.data('resource-id'));
+          }
+        });
+
       }
     }, this);
+  }
+
+  /* eslint-disable class-methods-use-this */
+  _getSelectedItem() {
+    const listController = getAppMain().getRootViewController()
+      .getPresentedViewController().getCurrentViewController()
+      .getCurrentController()._listController;
+    if (listController) {
+      return listController.getIterator().current();
+    }
+
+    const currentController = getAppMain().getRootViewController()
+      .getPresentedViewController().getCurrentViewController()
+      .getCurrentController()._rightController._currentController;
+    if (currentController && currentController._viewmodel) {
+      const current = currentController._viewmodel.current();
+
+      return current._item ? current._item : current;
+    }
+
+    return null;
   }
 
   deactivate(state) {
--- app/enhance-overral-ui/settings-entry.js
+++ app/enhance-overral-ui/settings-entry.js
@@ -4,5 +4,7 @@ export class EnhanceOverralUiSettings extends SettingsEntry {
   static id = 'enhance-overral-ui';
   constructor() {
     super('enhance-overral-ui', 'Enhance Overral UI');
+
+    this.addSetting('Show Google Link on Player Detail', 'show-google-link', false, 'checkbox');
   }
 }
