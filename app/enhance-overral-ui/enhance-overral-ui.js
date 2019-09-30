7f35aedcadf916eb3c8cef0f70ee9b82fd8253f1
Marosdee <marosdee@isidsea.com>
2019-09-30 15:15:39 +0700

    Implement Simple Condition.

--- app/enhance-overral-ui/enhance-overral-ui.js
+++ app/enhance-overral-ui/enhance-overral-ui.js
@@ -12,6 +12,9 @@ export class EnhanceOverralUi extends BaseScript {
 
   constructor() {
     super(EnhanceOverralUiSettings.id);
+
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    this._observer = new MutationObserver(this._mutationHandler.bind(this));
   }
 
   activate(state) {
@@ -25,6 +28,24 @@ export class EnhanceOverralUi extends BaseScript {
     if (settings.isActive) {
 
     }
+  }
+
+  _mutationHandler(mutationRecords) {
+    mutationRecords.forEach(function (mutation) {
+      if ($(mutation.target).hasClass('DetailView') && $(mutation.target)
+        .find('.DetailPanel') && mutation.addedNodes.length > 0) {
+        if (this.getSettings()['show-link-to-player'].toString() !== 'true') {
+          return;
+        }
+
+        let selectedItem = this._getSelectedItem();
+        if (selectedItem == null || selectedItem.resourceId === 0) {
+          return;
+        }
+
+        const futbinPlayerLink = $(mutation.target).find('#futbinPlayerLink');
+      }
+    }, this);
   }
 
   deactivate(state) {
